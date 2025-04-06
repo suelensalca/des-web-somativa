7a7e480f8f71376fa949a7330efe9acc221025ec
Suelen <suelen.salca@ringa.com.br>
2025-04-06 17:10:54 -0300

    Refactor app to add login access

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [mensagem, setMensagem] = useState("");
+
+  const handleSubmit = () => {
+    if (email === "eduardo.lino@pucpr.br" && password === "123456")
+      setMensagem("Acessado com sucesso!");
+    else
+      setMensagem("Usuário ou senha incorretos!");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Container'>
+      <p className="Title">
+        Login
+      </p>
+      <div>
+        <input className="Input" type="email" size={20} value={email} onChange={(e) => setEmail(e.target.value)} />
+      </div>
+      <div>
+        <input className="Input" type="password" size={20} value={password} onChange={(e) => setPassword(e.target.value)} />
+      </div>
+      <button className="Button" onClick={handleSubmit}>Acessar</button>
+      <p className="Message">{mensagem}</p>
     </div>
   );
 }
